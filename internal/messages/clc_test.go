2fb89471cb9a9518222c015140bb240e305034e1
hwipl <33433250+hwipl@users.noreply.github.com>
2020-02-23 15:16:57 +0100

    add clc proposal test to clc_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/messages/clc_test.go
+++ internal/messages/clc_test.go
@@ -6,6 +6,40 @@ import (
 	"testing"
 )
 
+func TestParseCLCHeaderProposal(t *testing.T) {
+	// prepare message
+	msg_bytes := "e2d4c3d901003410b1a098039babcdef" +
+		"fe800000000000009a039bfffeabcdef" +
+		"98039babcdef00007f00000008000000" +
+		"e2d4c3d9"
+	msg, err := hex.DecodeString(msg_bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse message
+	clc := ParseCLCHeader(msg)
+	clc.Parse(msg)
+
+	// check output message without reserved fields
+	want := "Proposal: Eyecatcher: SMC-R, Type: 1 (Proposal), " +
+		"Length: 52, Version: 1, Flag: 0, Path: SMC-R, " +
+		clc.proposal.String() + ", Trailer: SMC-R"
+	got := clc.String()
+	if got != want {
+		t.Errorf("clc.String() = %s; want %s", got, want)
+	}
+
+	// check output message with reserved fields
+	want = "Proposal: Eyecatcher: SMC-R, Type: 1 (Proposal), " +
+		"Length: 52, Version: 1, Flag: 0, Reserved: 0x0, " +
+		"Path: SMC-R, " + clc.proposal.Reserved() + ", Trailer: SMC-R"
+	got = clc.Reserved()
+	if got != want {
+		t.Errorf("clc.Reserved() = %s; want %s", got, want)
+	}
+}
+
 func TestParseCLCHeaderDecline(t *testing.T) {
 	// prepare message
 	msg_bytes := "e2d4c3d904001c102525252525252500" +
