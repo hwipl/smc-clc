a85adb113b5f772db533c5950ec8a24cf7793ec6
hwipl <33433250+hwipl@users.noreply.github.com>
2020-03-13 16:11:15 +0100

    add comments to buffer_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/buffer_test.go
+++ internal/cmd/buffer_test.go
@@ -10,11 +10,13 @@ func TestBuffer(t *testing.T) {
 	var want []byte
 	var got []byte
 
+	// test empty buffer
 	got = buf.copyBuffer().Bytes()
 	if !bytes.Equal(want, got) {
 		t.Errorf("buf = %s; want %s", got, want)
 	}
 
+	// test writing to empty buffer
 	want = []byte("hello world")
 	buf.Write(want)
 	got = buf.copyBuffer().Bytes()
@@ -22,6 +24,7 @@ func TestBuffer(t *testing.T) {
 		t.Errorf("buf = %s; want %s", got, want)
 	}
 
+	// test appending to buffer
 	buf.Write(want)
 	want = []byte("hello worldhello world")
 	got = buf.copyBuffer().Bytes()
@@ -29,6 +32,7 @@ func TestBuffer(t *testing.T) {
 		t.Errorf("buf = %s; want %s", got, want)
 	}
 
+	// test resetting buffer
 	buf.reset()
 	want = []byte("")
 	got = buf.copyBuffer().Bytes()
