0259a5f077a6482e6c085209959b3e811b7c82cb
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-01 16:13:20 +0200

    move http code into external module
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/google/gopacket v1.1.17
 	github.com/hwipl/packet-go v0.0.0-20200324140913-b3b354da9952
-	github.com/hwipl/smc-go v0.0.0-20200331192317-21f750acaf99
+	github.com/hwipl/smc-go v0.0.0-20200401135211-384d6b8d0667
 )
--- internal/cmd/http.go
+++ internal/cmd/http.go
@@ -1,35 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-
 	"github.com/hwipl/smc-go/pkg/util"
 )
 
-var (
-	httpBuffer util.Buffer
-)
-
-// printHTTP prints the httpBuffer to http clients
-func printHTTP(w http.ResponseWriter, r *http.Request) {
-	b := httpBuffer.CopyBuffer()
-	if _, err := io.Copy(w, b); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	flush := r.URL.Query().Get("flush")
-	if flush == "true" {
-		httpBuffer.Reset()
-	}
-}
-
 // setHTTPOutput sets the standard output to http and starts a http server
 func setHTTPOutput() {
-	stdout = &httpBuffer
-	stderr = &httpBuffer
-
-	http.HandleFunc("/", printHTTP)
-	go http.ListenAndServe(*httpListen, nil)
+	h := util.StartHTTPOutput(*httpListen)
+	stdout = &h.Buffer
+	stderr = &h.Buffer
 }
