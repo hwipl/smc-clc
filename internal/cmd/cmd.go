32447981d6721277c95139fc44daca7a1b3a6d10
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-12 12:51:28 +0200

    update description of command line arguments
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -9,16 +9,22 @@ import (
 
 var (
 	// pcap variables
-	pcapFile    = flag.String("f", "", "the pcap file to read")
-	pcapDevice  = flag.String("i", "", "the interface to listen on")
-	pcapPromisc = flag.Bool("pcap-promisc", true, "promiscuous mode")
-	pcapSnaplen = flag.Int("pcap-snaplen", 2048, "pcap snaplen in byte")
-	pcapTimeout = flag.Int("pcap-timeout", 0, "pcap timeout in ms")
-	pcapMaxPkts = flag.Int("pcap-maxpkts", 0, "maximum packets to "+
-		"capture (may require pcap-timeout argument)")
-	pcapMaxTime = flag.Int("pcap-maxtime", 0, "maximum capturing time "+
-		"in s (may require pcap-timeout argument)")
-	pcapFilter = flag.String("pcap-filter", "", "pcap packet filter")
+	pcapFile = flag.String("f", "",
+		"read packets from a pcap file and set it to `file`")
+	pcapDevice = flag.String("i", "", "read packets from "+
+		"a network interface (default) and set it to `interface`")
+	pcapPromisc = flag.Bool("pcap-promisc", true,
+		"set network interface to promiscuous mode")
+	pcapSnaplen = flag.Int("pcap-snaplen", 2048,
+		"set pcap snaplen to `bytes`")
+	pcapTimeout = flag.Int("pcap-timeout", 0,
+		"set pcap timeout to `milliseconds`")
+	pcapMaxPkts = flag.Int("pcap-maxpkts", 0, "set maximum packets to "+
+		"capture to `number` (may require pcap-timeout argument)")
+	pcapMaxTime = flag.Int("pcap-maxtime", 0, "set maximum capturing "+
+		"time to `seconds` (may require pcap-timeout argument)")
+	pcapFilter = flag.String("pcap-filter", "",
+		"set pcap packet filter to `filter` (e.g.: \"not port 22\")")
 
 	// display variables
 	showReserved = flag.Bool("show-reserved", false,
@@ -30,8 +36,9 @@ var (
 	// output, changed by http output
 	stdout     io.Writer = os.Stdout
 	stderr     io.Writer = os.Stderr
-	httpListen           = flag.String("http", "",
-		"use http server and set listen address (e.g.: :8000)")
+	httpListen           = flag.String("http", "", "use http server "+
+		"output and listen on `address` "+
+		"(e.g.: :8000 or 127.0.0.1:8080)")
 )
 
 // Run is the main entry point of the smc-clc program: it parses the command
